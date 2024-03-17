226f4edc7c4e22bfbde1c7cb162a7a507b6a6e89
Sergio Costa <sergio.costa@ufma.br>
2024-03-17 16:53:12 -0300

    update

--- Dashboard.py
+++ Dashboard.py
@@ -1,12 +1,12 @@
 
 import streamlit as st
-import requests
-import pandas as pd
-import plotly.express as px
+#import requests
+#import pandas as pd
+#import plotly.express as px
 
 import datadotworld as dw
 
-st.title('DbAcademic')
+st.title('DbAcademic .... ')
 
 st.sidebar.title('Filtros')
 nome_curso = st.sidebar.text_input("Nome do Curso")
